7fbc14be0a9a24cb97f19b036d4fad8a63f45b5b
Dudu! <83459496+dudurtg2@users.noreply.github.com>
2023-11-30 21:52:46 -0300

    Codigo base para quartos do hotel

--- alpha.0.0.1.cpp
+++ alpha.0.0.1.cpp
@@ -5,6 +5,7 @@
 #include <string.h>
 
 #define MAX_CLIENTES 100
+#define MAX_QUARTOS 100
 
 typedef struct {
   char nome[50];
@@ -13,52 +14,77 @@ typedef struct {
 } Cliente;
 
 typedef struct {
-  Cliente clientes[MAX_CLIENTES];
   int numero;
-} ListaClientes;
+  int capacidade;
+  float preco;
+} Quarto;
 
-void cadastrarCliente(ListaClientes *lista);
-void exibirClientes(ListaClientes lista);
-void menu(ListaClientes *lista);
+typedef struct {
+  Cliente clientes[MAX_CLIENTES];
+  Quarto quartos[MAX_QUARTOS];
+  int numClientes;
+  int numQuartos;
+} Hotel;
 
-void cadastrarCliente(ListaClientes *lista) {
+void cadastrarCliente(Hotel *hotel);
+void cadastrarQuarto(Hotel *hotel);
+void exibirClientes(Hotel hotel);
+void menu(Hotel *hotel);
+
+void cadastrarCliente(Hotel *hotel) {
   fflush(stdin);
   system("cls");
   printf("Digite o nome do cliente: ");
-  fgets(lista->clientes[lista->numero].nome, sizeof(lista->clientes[lista->numero].nome), stdin);
+  fgets(hotel->clientes[hotel->numClientes].nome, sizeof(hotel->clientes[hotel->numClientes].nome), stdin);
   printf("Digite o endereço do cliente: ");
-  fgets(lista->clientes[lista->numero].endereco, sizeof(lista->clientes[lista->numero].endereco), stdin);
+  fgets(hotel->clientes[hotel->numClientes].endereco, sizeof(hotel->clientes[hotel->numClientes].endereco), stdin);
   printf("Digite o telefone do cliente: ");
-  fgets(lista->clientes[lista->numero].telefone, sizeof(lista->clientes[lista->numero].telefone), stdin);
-  lista->numero++;
-  menu(lista);
+  fgets(hotel->clientes[hotel->numClientes].telefone, sizeof(hotel->clientes[hotel->numClientes].telefone), stdin);
+  hotel->numClientes++;
+  menu(hotel);
 }
 
-void exibirClientes(ListaClientes lista) {
-  for (int i = 0; i < lista.numero; i++)
+void cadastrarQuarto(Hotel *hotel) {
+  fflush(stdin);
+  system("cls");
+  printf("Digite o número do quarto: ");
+  scanf("%d", &hotel->quartos[hotel->numQuartos].numero);
+  printf("Digite a capacidade do quarto: ");
+  scanf("%d", &hotel->quartos[hotel->numQuartos].capacidade);
+  printf("Digite o preço do quarto: ");
+  scanf("%f", &hotel->quartos[hotel->numQuartos].preco);
+  hotel->numQuartos++;
+  menu(hotel);
+}
+
+void exibirClientes(Hotel hotel) {
+  for (int i = 0; i < hotel.numClientes; i++)
   {
-    printf("Nome: %s", lista.clientes[i].nome);
+    printf("Nome: %s", hotel.clientes[i].nome);
   }
-  menu(&lista);
+  menu(&hotel);
 }
 
 int main() {
-  ListaClientes lista;
-  lista.numero = 0;
-  menu(&lista);
+  Hotel hotel;
+  hotel.numClientes = 0;
+  hotel.numQuartos = 0;
+  menu(&hotel);
   return 0;
 }
 
-void menu(ListaClientes *lista) {
+void menu(Hotel *hotel) {
   int opcao = 0;
   printf("Escolha:\n");
   scanf("%d", &opcao);
 
   do {
     if (opcao == 1) {
-      cadastrarCliente(lista);
+      cadastrarCliente(hotel);
     } else if (opcao == 2) {
-      exibirClientes(*lista);
+      exibirClientes(*hotel);
+    } else if (opcao == 3) {
+      cadastrarQuarto(hotel);
     }
 
   } while (true);
